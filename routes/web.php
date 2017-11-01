f5ecd5a0de3cd77fe2c56d44b4e24cb02e31776f
Ak-0 <akhos007@fiu.edu>
2017-11-01 18:53:38 -0400

    adding web.php

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+use App\User;
+	Route::get('/', function () {
+		$addresses=\App\Address::all();
+		return View('addresses',['address'=>$addresses]);
+	});
+
+
+//THE ADDRESSES APP
+
+
+	Route::get('address', ['uses' => 'Addresses@index']);
+	Route::get('add_address', ['uses' => 'Addresses@add_address']);
+	Route::post('add_address', ['uses' => 'Addresses@add_address']);
+ 
+	Route::get('address/edit/{id}', ['uses' => 'Addresses@edit']);
+	Route::post('address/edit/{id}', ['uses' => 'Addresses@edit_address']);
+ 
+	Route::get('address/delete/{id}', ['uses' => 'Addresses@delete']);
+ 
+	Route::get('address/sort/{any}', ['uses' => 'Addresses@sort']);
+ 
+	Route::post('address/filter/', ['uses' => 'Addresses@filter']);
